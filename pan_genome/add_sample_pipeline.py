e0752c11dd16cd745cf160c245dbf8ae20db8d20
ntanh1999 <ntanh1999@gmail.com>
2021-07-10 08:17:35 +0000

    cd-hit-2d -s2

--- pan_genome/add_sample_pipeline.py
+++ pan_genome/add_sample_pipeline.py
@@ -12,7 +12,7 @@ def run_cd_hit_2d(database_1, database_2, out_dir, threads=4, timing_log=None):
     not_match_fasta = os.path.join(out_dir, 'cd-hit-2d.fasta')
     cd_hit_cluster_file = not_match_fasta + '.clstr'
     
-    cmd = f'cd-hit-2d -i {database_1} -i2 {database_2} -o {not_match_fasta} -s 0.98 -s2 0.98 -c 0.98 -T {threads} -M 0 -g 1 -d 256 > /dev/null'
+    cmd = f'cd-hit-2d -i {database_1} -i2 {database_2} -o {not_match_fasta} -s 0.98 -c 0.98 -T {threads} -M 0 -g 1 -d 256 > /dev/null'
     ret = run_command(cmd, timing_log)
     if ret != 0:
         raise Exception('Error running cd-hit-2d')

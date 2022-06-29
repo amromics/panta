ce3ecd27b618406d5449062eaa09e4ca3ec2dfd0
ntanh1999 <ntanh1999@gmail.com>
2022-06-29 01:57:27 +0000

    create msa

--- scripts/create_msa.py
+++ scripts/create_msa.py
@@ -92,6 +92,13 @@ def split_random(seq_list, ratio):
     second_seqs = seq_list[first_seqs_len:]
     return first_seqs, second_seqs
 
+def split_num(seq_list, num):
+    random.seed(62)
+    random.shuffle(seq_list)
+    first_seqs = seq_list[:num]
+    second_seqs = seq_list[num:]
+    return first_seqs, second_seqs
+
 def split_seq_file(seq_file):
     seq_list = []
     with open(seq_file, 'r') as fh:
@@ -103,7 +110,8 @@ def split_seq_file(seq_file):
     seq_list.sort(key= lambda x:len(x[1])) # sort sequences by length
     
     # first_seqs, second_seqs = split_random(seq_list, 0.2)
-    first_seqs, second_seqs = split_by_length(seq_list, 0.2)
+    # first_seqs, second_seqs = split_by_length(seq_list, 0.2)
+    first_seqs, second_seqs = split_num(seq_list, 500)
 
     mafft_seq = os.path.join(seq_file + '.1')
     with open(mafft_seq, 'w') as fh:
@@ -125,7 +133,7 @@ def new_method(seq_file, msa_file):
 
     tmp_msa_file = msa_file + '.tmp'
     cmd = ("mafft --localpair --maxiterate 1000 --quiet --thread 1 "
-           f"{mafft_seq} > {tmp_msa_file}")
+           f" {mafft_seq} > {tmp_msa_file}")
     run_command(cmd)
 
     cmd = (f'abpoa {poa_seq} -i {tmp_msa_file} -o {msa_file} -r1 '
@@ -158,13 +166,26 @@ def run_qscore(test_file, ref_file):
     else:
         SPS = result.group(1)
         CS = result.group(2)
-        print(basename_1, basename_2, SPS, CS, sep='\t')
+        # print(basename_1, basename_2, SPS, CS, sep='\t')
         return SPS, CS
 
 
 def qscore_parallel(test_list, reference_list):
     with multiprocessing.Pool(processes=threads) as pool:
-        pool.starmap(run_qscore, zip(test_list, reference_list))
+        results = pool.starmap(run_qscore, zip(test_list, reference_list))
+    
+    sum_SP = 0
+    sum_TC = 0
+    for SP, TC in results:
+        sum_SP += float(SP)
+        sum_TC += float(TC)
+    
+    ave_SP = round(sum_SP / len(results), 4)
+    ave_TC = round(sum_TC / len(results), 4)
+
+    print('Average SP', ave_SP, sep=',')
+    print('Average TC', ave_TC, sep=',')
+
 
 def pairwise_score(S1, S2):
     score = 0
@@ -204,14 +225,15 @@ def msa_score(msa_file):
     print(base_name, int(msa_score), sep='\t')
 
 def run_iqtree_alisim():
-    root_name = 'test'
+    os.chdir('/home/ntanh1999/simulation')
+    
+    root_name = 'test_12'
     model = '-m LG '
     indel = '--indel 0.05,0.05 '
-    random_tree = '-t RANDOM{yh/100} -rlen 0.001 0.05 0.999 '
-    seq_length = '--length 1000 '
-    number_msa = '--num-alignments 3'
+    random_tree = '-t RANDOM{yh/2000} -rlen 0.001 0.01 0.999 '
+    seq_length = '--length 400 '
+    number_msa = '--num-alignments 8'
 
-    os.chdir('/home/noideatt/TA/simulate')
     cmd = (f"iqtree2 --alisim {root_name} -af fasta " 
             + model + indel + random_tree 
             + seq_length + number_msa)
@@ -247,16 +269,38 @@ def calucate_average_identity(msa_file):
     
     identity_score = sum(results) / len(results)
 
-    print(base_name, round(identity_score,2), sep='\t')
+    # print(base_name, round(identity_score,2), sep='\t')
+    return identity_score
 
-if __name__ == "__main__":
-	
-    global threads
-    threads = 8
-
-    global matrix_file # Blosum matrix file for POA
-    matrix_file = '/home/noideatt/TA/pan-genome/BLOSUM62.mtx'
+def calucate_average_identity_2(msa_file):
+    align = AlignIO.read(msa_file, "fasta")
+    base_name = os.path.basename(msa_file)
     
+    pair = []
+    S1 = align[0]
+    for S2 in align:
+        pair.append((S1,S2))
+
+    with multiprocessing.Pool(processes=threads) as pool:
+        results = pool.starmap(pairwise_identity, pair)
+    
+    identity_score = sum(results) / len(results)
+
+    # print(base_name, round(identity_score,2), sep='\t')
+    return identity_score
+
+def calculate_average_identity(ref_list):
+    identity_ls = []
+    for ref_msa in ref_list:
+        identity = calucate_average_identity_2(ref_msa)
+        identity_ls.append(identity)
+    ave_identity = round(sum(identity_ls)/len(identity_ls), 2)
+    print('Average Identity', ave_identity, sep=',')
+
+
+def main_1():
+    global matrix_file # Blosum matrix file for POA
+    matrix_file = '/home/ntanh1999/amromics/amromics/pan-genome/BLOSUM62.mtx'
     global matrix
     matrix = substitution_matrices.load("BLOSUM62")
     global gop
@@ -264,14 +308,13 @@ if __name__ == "__main__":
     global gep
     gep = -1
 
+    root_name = run_iqtree_alisim()
+    
     global timing_log
-    timing_log = '/home/noideatt/TA/simulate/time.log'
-
-    # root_name = run_iqtree_alisim()
-    root_name = 'test'
+    timing_log = f'/home/ntanh1999/simulation/{root_name}.log'
      
     unaligned_seqs_list = glob(
-        f'/home/noideatt/TA/simulate/{root_name}*.unaligned.fa')
+        f'/home/ntanh1999/simulation/{root_name}_*.unaligned.fa')
 
     
     ref_list = []
@@ -293,20 +336,36 @@ if __name__ == "__main__":
         mafft_msa_list.append(mafft_msa)
         new_method_list.append(new_method_msa)
     
-    for ref_msa in ref_list:
-        calucate_average_identity(ref_msa)
-    # create_poa_in_parallel(unaligned_seqs_list, poa_msa_list)
+    calculate_average_identity(ref_list)
+
+    create_poa_in_parallel(unaligned_seqs_list, poa_msa_list)
     # create_mafft_in_parallel(unaligned_seqs_list, mafft_msa_list)
-    # new_method_parallel(unaligned_seqs_list, new_method_list)
+    new_method_parallel(unaligned_seqs_list, new_method_list)
 
     qscore_parallel(poa_msa_list, ref_list)
-    qscore_parallel(mafft_msa_list, ref_list)
+    # qscore_parallel(mafft_msa_list, ref_list)
     qscore_parallel(new_method_list, ref_list)
 
 
-    for msa in mafft_msa_list:
-        msa_score(msa)
-    for msa in poa_msa_list:
-        msa_score(msa)
-    for msa in new_method_list:
-        msa_score(msa)
+    # for msa in mafft_msa_list:
+    #     msa_score(msa)
+    # for msa in poa_msa_list:
+    #     msa_score(msa)
+    # for msa in new_method_list:
+    #     msa_score(msa)
+    # for msa in ref_list:
+    #     msa_score(msa)
+
+
+def main_2():
+    for i in range(0, 5001):
+        msa_path = f'/home/ntanh1999/evaluate_msa/Pa100/{str(i)}/{str(i)}.mafft'
+        calucate_average_identity_2(msa_path)
+
+if __name__ == "__main__":
+	
+    global threads
+    threads = 8
+
+    main_1()
+    # main_2()

ce5280daad53133a78e18dae42639d9db1eb1265
Lê Đức Quang <leducquangpm@gmail.com>
2025-05-04 08:50:38 +0700

    add command to run

--- run.sh
+++ run.sh
@@ -41,21 +41,31 @@ pip install .
 # /usr/bin/time -v  panta add -a protein --poa --dont-split -c out/Sp600/m_ref_g_mmseq_a_diamond_c_mcl_4_123 -g /media/ktht/Store/Quang/bio/CollectionPanta/TestInQualifiedGFF/Sp600_batches/Sp600/batch_gff_123/*.gff >> run-m_ref_g_mmseq_a_diamond_c_mcl-Sp600-add-4_123.txt 2>&1
 #/usr/bin/time -v  panta main  --dont-split -o  out/Kp1500/g_cdhit_a_diamond_c_mcl_Kp1500_12345 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_1/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_2/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_3/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_4/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_5/*.gff>> run-g_cdhit_a_diamond_c_mcl-Kp1500-12345.txt 2>&1
 #pip install .
-# rm run-g_mmseq_a_diamond_c_mcl_unique3.1-Kp1500-1.txt 2>&1
-# rm run-g_mmseq_a_diamond_c_mcl_unique3.1-Kp1500-add-12.txt
-# rm run-g_mmseq_a_diamond_c_mcl_unique3.1-Kp1500-add-123.txt
-# # # rm run-g_mmseq_a_diamond_c_mcl_unique-Kp1500-add-1234.txt
-# # # rm run-g_mmseq_a_diamond_c_mcl_unique2-Kp1500-add-12345.txt
+rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-1.txt 2>&1
+rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-12.txt
+rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-123.txt
+rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-1234.txt
+rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-12345.txt
 #rm run-g_mmseq_a_diamond_c_mcl_unique_cc-Kp1500-12345.txt
 
 #rm -r out/Kp1500/g_mmseq_a_diamond_c_mcl_unique_cc_123_45
 #rm -r out/Kp1500/g_mmseq_a_diamond_c_mcl_unique_cc_1234_5
-# rm -r out/Kp1500/g_mmseq_a_diamond_c_mcl_unique3_1_2_3_4_5
+rm -r out/Kp1500/g_mmseq_a_diamond_c_mcl_hash_opt2
 
 #/usr/bin/time -v  panta main -m g_faiss_a_faiss_c_mcl   --dont-split -o  out/Kp1500/g_faiss_a_faiss_c_mcl -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_1/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_2/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_3/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique_cc-Kp1500-123.txt 2>&1
-# /usr/bin/time -v  panta main -m g_mmseq_a_diamond_c_mcl_cc   --dont-split -o  out/Kp1500/g_mmseq_a_diamond_c_mcl_unique3_1_2_3_4_5 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_1/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique3.1-Kp1500-1.txt 2>&1
+/usr/bin/time -v  panta main -m g_mmseq_a_diamond_c_mcl_hash_opt2   --dont-split -o  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_1/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-1.txt 2>&1
 
-#/usr/bin/time -v  panta add --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_unique3_1_2_3_4_5 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_2/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique3.1-Kp1500-add-12.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash_opt2   --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_2/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-12.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash_opt2 --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_3/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-123.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash_opt2 --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_4/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-1234.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash_opt2 --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_5/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-Kp1500-add-12345.txt 2>&1
+
+/usr/bin/time -v  panta main -m g_mmseq_a_diamond_c_mcl_hash  --dont-split -o  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_1/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash-Kp1500-1.txt 2>&1
+
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_2/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash-Kp1500-add-12.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_3/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash-Kp1500-add-123.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_4/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash-Kp1500-add-1234.txt 2>&1
+/usr/bin/time -v  panta add -m g_mmseq_a_diamond_c_mcl_hash --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_hash -g /mnt/data/data/amromics/panta2/panta/examples/Kp1500/batch_5/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash-Kp1500-add-12345.txt 2>&1
 # # # #/usr/bin/time -v  panta add --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_unique3_1_2_3_4_5 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_2/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique3-Kp1500-add-12.txt 2>&1
 # /usr/bin/time -v  panta add --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_unique3_1_2_3_4_5 -g  /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_3/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique3.1-Kp1500-add-123.txt 2>&1
 #/usr/bin/time -v  panta add  --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_unique_cc_123_45 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_4/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_5/*.gff>> run-g_mmseq_a_diamond_c_mcl_unique_cc-Kp1500-add-123_45.txt 2>&1
@@ -66,20 +76,20 @@ pip install .
 #/usr/bin/time -v  panta main -m g_mmseq_a_diamond_c_mcl_cc --dont-split -o out/Kp1500/g_mmseq_a_diamond_c_mcl_unique_cc_1234_5 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_1/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_2/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_3/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_4/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique_cc-Kp1500-1234.txt 2>&1
 #/usr/bin/time -v  panta add  --dont-split -c  out/Kp1500/g_mmseq_a_diamond_c_mcl_unique_cc_1234_5 -g /media/ktht/ExtraUbuntu/amromics/panta2/Kp1500/batch_5/*.gff>> run-g_mmseq_a_diamond_c_mcl_unique_cc-Kp1500-add-1234_5.txt 2>&1
 #panaroo --merge_paralogs -t 20 --clean-mode strict
-#rm run-g_mmseq_a_ems_c_mcl_unique-test-main.txt
-rm run-g_mmseq_a_ems_c_mcl_unique-test-all.txt
-#rm run-g_mmseq_a_diamond_c_mcl_unique5-test-add-1.txt
-#rm run-g_mmseq_a_diamond_c_mcl_unique5-test-add-2.txt
-#rm run-g_mmseq_a_diamond_c_mcl_unique5-test-add-3.txt
-rm -r out/test/g_mmseq_a_ems_c_mcl_unique
+#rm  run-g_mmseq_a_diamond_c_mcl_hash_opt2-test-main.txt
+#rm run-g_mmseq_a_ems_c_mcl_unique-test-all.txt
+#rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-test-add-1.txt
+#rm run-g_mmseq_a_diamond_c_mcl_hash_opt2-test-add-2.txt 2>&1
+#rm run-g_mmseq_a_diamond_c_mcl_hash_old-test-add-3.txt
+#rm -r out/test/g_mmseq_a_diamond_c_mcl_hash_opt2
 #rm -r out/test/g_mmseq_a_diamond_c_mcl_unique5_all
 #rm -r out/test/g_mmseq_a_diamond_c_mcl_unique5_12
-#/usr/bin/time -v  panta main -t 12 -m g_mmseq_a_ems_c_mcl  --dont-split  -o out/test/g_mmseq_a_ems_c_mcl_unique -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/main/*.gff >> run-g_mmseq_a_ems_c_mcl_unique-test-main.txt 2>&1
-/usr/bin/time -v  panta main -t 12 -m g_mmseq_a_ems_c_mcl  --dont-split  -o out/test/g_mmseq_a_ems_c_mcl_unique -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/main/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add2/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add3/*.gff >> run-g_mmseq_a_ems_c_mcl_unique-test-all.txt 2>&1
+#/usr/bin/time -v  panta main -t 8 -m g_mmseq_a_diamond_c_mcl_hash_opt2  --dont-split  -o out/test/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/test/main/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-test-main.txt 2>&1
+#usr/bin/time -v  panta main -t 12 -m g_mmseq_a_ems_c_mcl  --dont-split  -o out/test/g_mmseq_a_ems_c_mcl_unique -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/main/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add2/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add3/*.gff >> run-g_mmseq_a_ems_c_mcl_unique-test-all.txt 2>&1
 
-#/usr/bin/time -v  panta add --dont-split -c  out/test/g_mmseq_a_diamond_c_mcl_unique5 -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique5-test-add-1.txt 2>&1
-#/usr/bin/time -v  panta add --dont-split -c  out/test/g_mmseq_a_diamond_c_mcl_unique5 -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add2/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique5-test-add-2.txt 2>&1
-#/usr/bin/time -v  panta add --dont-split -c  out/test/g_mmseq_a_diamond_c_mcl_unique5 -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add3/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique5-test-add-3.txt 2>&1
+#/usr/bin/time -v  panta add -t 8 -m g_mmseq_a_diamond_c_mcl_hash_opt2 --dont-split -c  out/test/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/test/add/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-test-add-1.txt 2>&1
+#/usr/bin/time -v  panta add -t 8 -m g_mmseq_a_diamond_c_mcl_hash_opt2 --dont-split -c  out/test/g_mmseq_a_diamond_c_mcl_hash_opt2 -g /mnt/data/data/amromics/panta2/panta/examples/test/add2/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-test-add-2.txt 2>&1
+#/usr/bin/time -v  panta add -t 8 -m g_mmseq_a_diamond_c_mcl_hash --dont-split -c  out/test/g_mmseq_a_diamond_c_mcl_hash_old -g /mnt/data/data/amromics/panta2/panta/examples/test/add3/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_old-test-add-3.txt 2>&1
 #/usr/bin/time -v  panta main -t 12 -m g_mmseq_a_diamond_c_mcl_cc  --dont-split  -o out/test/g_mmseq_a_diamond_c_mcl_unique5_all -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/main/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add2/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add3/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique5-test-all.txt 2>&1
 #/usr/bin/time -v  panta main -t 12 -m g_mmseq_a_diamond_c_mcl_cc  --dont-split  -o out/test/g_mmseq_a_diamond_c_mcl_unique5_12 -g /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/main/*.gff /media/ktht/ExtraUbuntu/amromics/panta2/panta/examples/test/add/*.gff >> run-g_mmseq_a_diamond_c_mcl_unique5-test-12.txt 2>&1
 
@@ -104,3 +114,7 @@ rm -r out/test/g_mmseq_a_ems_c_mcl_unique
 #/usr/bin/time -v panta add -t 12 --dont-split -c out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5 -g /media/ktht/ExtraUbuntu/amromics/panta2/Ecoli/S13/*gff.gz >> run-unique5.1_ecoli_s13.txt 2>&1
 #/usr/bin/time -v ./diamond blastp -q out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5/temp/reduced_new_unique_seqs.fasta -d out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5/representative_clusters.fasta -p 12 --evalue 1e-06 --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen --sensitive --max-target-seqs 2000 > out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5/temp/matching_new_sequences_diamond.tsv  >> run-diamondblast_reduced_new_unique_seqs.txt 2>&1
 #/usr/bin/time -v ./diamond blastp -q out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5/representative_clusters.fasta -d out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5/representative_clusters.fasta -p 4 --evalue 1e-6 --outfmt 6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen --sensitive --max-target-seqs 2000 2> /dev/null 1> out/Ecoli30k/g_mmseq_a_diamond_c_mcl_unique5/pairwise_rep_clusters_diamond_test.tsv >> run-diamondblast_representative.txt 2>&1
+#/usr/bin/time -v  panta main -t 8 -m g_mmseq_a_diamond_c_mcl_hash_opt2  --dont-split  -o out/sp600/g_mmseq_a_diamond_c_mcl_hash_opt2 -g examples/Sp600/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-sp600-main.txt 2>&1
+#/usr/bin/time -v  panta add -t 8 -m g_mmseq_a_diamond_c_mcl_hash_opt2 --dont-split -c  out/sp600/g_mmseq_a_diamond_c_mcl_hash_opt2 -g examples/Sp300/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_opt2-sp600-add.txt 2>&1
+#/usr/bin/time -v  panta main -t 8 -m g_mmseq_a_diamond_c_mcl_hash  --dont-split  -o out/sp600/g_mmseq_a_diamond_c_mcl_hash_old -g examples/Sp600/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_old-sp600-main.txt 2>&1
+#/usr/bin/time -v  panta add -t 8 -m g_mmseq_a_diamond_c_mcl_hash --dont-split -c  out/sp600/g_mmseq_a_diamond_c_mcl_hash_old -g examples/Sp300/*.gff >> run-g_mmseq_a_diamond_c_mcl_hash_old-sp600-add.txt 2>&1
